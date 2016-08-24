0ab7f699f5cf62ff4927f2c6b3cdb78fa3c0a445
Aaron Gullickson <aarong@uoregon.edu>
2016-08-24 14:11:32 -0700

    change entropy scale in python script

--- entropy.py
+++ entropy.py
@@ -15,16 +15,23 @@ sys.path.append('./thoth/')
 import thoth.thoth as thoth
 dir(thoth)
 import numpy as np
+import math as math
+import csv
+
+#a scalar to multiply entropy by to bring it into a [0,1] range
+scale =  1/(math.log(7,2))
+#1990 data requires a different scale because there were only six categories (no multiracial)
+scale90 = 1/(math.log(6,2))
 
 ##CALCULATE ENTROPY VIA BOOSTRAP FOR DETAILED ANCESTRIES##
 #read in the CSV file and calculate entropy
-import csv
 ancestry = np.array([['ancestry','white','black','indig','asian','latino','mixed','other','N','meso','big','entropy','eplusone','eminusone','eplustwo','eminustwo']])
 with open('output/raceancestryd_single_2000_weighted.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		a = [float(row['white']), float(row['black']), float(row['indig']), float(row['asian']), float(row['latino']), float(row['mixed']), float(row['other'])]
 		result = thoth.calc_entropy(a,10000)
+                result = [x * scale for x in result]
                 p = [[row['ancestr1d'],row['white'],row['black'],row['indig'],row['asian'],row['latino'],row['mixed'],row['other'],row['raceshort'],row['rancestry1'],row['rancestry1s'],str(result[0]),str(result[1]),str(result[2]),str(result[3]),str(result[4])]]
                 print p
                 ancestry = np.append(ancestry,p,0)                
@@ -41,6 +48,7 @@ with open('output/raceancestryomb_2000_weighted.csv') as csvfile:
         for row in reader:
                 a = [float(row['white']), float(row['black']), float(row['indig']), float(row['asian']), float(row['latino']), float(row['mixed']), float(row['other'])]
                 result = thoth.calc_entropy(a,1000)
+                result = [x * scale for x in result]
                 p = [[row['raceancd'],row['white'],row['black'],row['indig'],row['asian'],row['latino'],row['mixed'],row['other'],row['raceshort'],str(result[0]),str(result[1]),str(result[2]),str(result[3]),str(result[4])]]
                 print p
                 ancestry = np.append(ancestry,p,0)
@@ -49,13 +57,18 @@ with open('output/entropy_omb_2000.csv', 'wb') as csvfile:
         for row in ancestry:
                 writer.writerow(row)
 
-##CALCULATE ENTROYP VIA BOOTSTRAP FOR BIG RACE CATEOGORIES BY YEAR##
+##CALCULATE ENTROPY VIA BOOTSTRAP FOR BIG RACE CATEOGORIES BY YEAR##
 ancestry = np.array([['rancestry','year','white','black','indig','asian','latino','mixed','other','N','entropy','eplusone','eminusone','eplustwo','eminustwo']])
 with open('output/raceancestryomb_byyear_weighted.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
                 a = [float(row['white']), float(row['black']), float(row['indig']), float(row['asian']), float(row['latino']), float(row['mixed']), float(row['other'])]
+                year = int(row['yearint'])
                 result = thoth.calc_entropy(a,1000)
+                if year == 1990:
+                        result = [x * scale90 for x in result]
+                else:
+                        result = [x * scale for x in result]
                 p = [[row['raceancd'],row['yearint'],row['white'],row['black'],row['indig'],row['asian'],row['latino'],row['mixed'],row['other'],row['raceshort'],str(result[0]),str(result[1]),str(result[2]),str(result[3]),str(result[4])]]
                 print p
                 ancestry = np.append(ancestry,p,0)
